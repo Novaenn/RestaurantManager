16637fe57dfadd0a60911b5d453761ded13855d5
FraDepe <83709470+FraDepe@users.noreply.github.com>
2021-08-18 20:03:00 +0200

    Aggiunto scontrino (stringa formattata bene) e modificato ordine

--- RestaurantManager/src/Ordine.java
+++ RestaurantManager/src/Ordine.java
@@ -10,6 +10,13 @@ public class Ordine {
 		prezzoTot = 0;
 		piattiOrdinati = new ArrayList<>();
 	}
+	
+	//public static void main(String[] args) {
+	//	Ordine ciao = new Ordine();
+	//	ciao.addPiattoOrdinato(new Piatto("Gatto", 25));
+	//	ciao.addPiattoOrdinato(new Piatto("Capra", 17));
+	//	System.out.println(ciao.formattaOrdine());
+	//}
 
 	public void addPiattoOrdinato(Piatto nPiatto) {
 		piattiOrdinati.add(nPiatto);
@@ -21,7 +28,7 @@ public class Ordine {
 		prezzoTot -= piattiOrdinati.get(numero).getPrezzo();
 	}
 	
-	public int getPrezzoTot() {
+	public float getPrezzoTot() {
 		return prezzoTot;
 	}
 
@@ -37,4 +44,15 @@ public class Ordine {
 		this.piattiOrdinati = piattiOrdinati;
 	}
 	
+	public String formattaOrdine() {
+		String stringaOrdine = "";
+		for (int i = 0; i < piattiOrdinati.size(); i++) {
+			stringaOrdine +=String.format("%-25s %5.2f", piattiOrdinati.get(i).getNome(), piattiOrdinati.get(i).getPrezzo());
+			stringaOrdine += "\n";
+		}
+		stringaOrdine += "\n";
+		stringaOrdine += "\n-------------------------------- \n";
+		stringaOrdine += String.format("Totale:%24.2f ", this.getPrezzoTot());
+		return stringaOrdine;
+	}
 }
---
+++ RestaurantManager/src/Scontrino.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Scontrino {
+
+	private String scontrino;
+	
+	public Scontrino() {
+		scontrino = "";
+	}
+	
+	
+}
